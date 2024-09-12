746ecc3768a228085f47f7a2a34b3261d99cf065
BrunoLFSantos <brunoleonardofsantos@gmail.com>
2024-09-12 15:31:35 -0300

    Alinhando o layout da lista de transação

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(ExpensesApp());
@@ -31,7 +32,6 @@ class MyHomePage extends StatelessWidget {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           Container(
             child: Card(
@@ -42,11 +42,81 @@ class MyHomePage extends StatelessWidget {
             child: Column(
               children: _transaction.map((tr){
                 return Card(
-                  child: Text(tr.title),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2)
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text('R\$ ${tr.value.toStringAsFixed(2)}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple
+                        ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tr.title,
+                            style:  TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          Text(
+                            DateFormat('d MMM y').format(tr.date),
+                            style: TextStyle(
+                              color: Colors.grey
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 );
               }).toList(),
             ),
-          )
+          ),
+           Padding(
+             padding: const EdgeInsets.all(0.0),
+             child: Card(
+                elevation: 5,
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Título'
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Valor (R\$)'
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          onPressed: (){},
+                          child: Text('Nova Transação', 
+                            style: TextStyle(
+                              color: Colors.purple
+                            ),),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+           ),
         ],
       ),
     );
